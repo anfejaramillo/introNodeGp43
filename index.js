75357539a3bb75522c8457e12f236ee671451ce0
anfejaramillo <anfejaramillo@utp.edu.co>
2022-10-24 20:27:46 -0500

    commit 24 10

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const axios = require("axios");
 let urlCompleted =
     "https://api.openweathermap.org/data/2.5/weather?q=Bogota, col&appid=8e6330a78aa20b41b54f3fcaa3e9994f";
 
+let url2 = "https://catfact.ninja/fact";
+
 let requestConfig = {
     url: urlCompleted,
+};
+
+let requestConfig2 = {
+    url: url2,
 };
 
 let objPromesa = axios(requestConfig);
@@ -18,5 +24,15 @@ objPromesa
         );
     })
     .catch((err) => {
+        console.log(err);
+    });
+
+let requestPromise2 = axios(requestConfig2);
+
+requestPromise2
+    .then(function (response) {
+        console.log(response.data);
+    })
+    .catch(function (err) {
         console.log(err);
     });
